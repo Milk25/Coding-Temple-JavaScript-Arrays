9ba2632ee5696252f6c70a22fa80e7649d5b0185
milk19948 <97678058+Milk25@users.noreply.github.com>
2024-07-21 15:45:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+let fruits = ['apple', 'banana', 'orange']
+
+let newFruitBasket = fruit.push('Lemon')
+
+let newestFruitBasket = fruits.pop([fruits] - 2)
+
+let randomNumbers = [3, 1, 5, 2, 4]
+
+let newAssortedNumbers = randomNumbers.sort()
+
+
+
+let numbers = [3, 1, 5, 2, 4]
+
+let doubleEachNumber = numbers.filter().values("Even")
+
+let calculateSum = numbers.reduce()
+
+console.log(doubleEachNumber)
+
+
+
+
+let message = "Hello, World!"
+
+let stringLength = message.length()
+console.log(stringLength)
+
+let upperCase = message.toUpperCase()
+let lowerCase = message.toLowerCase()
+console.log(upperCase, lowerCase)
+
+
+
+
+
+let sentence = "The quick brown fox jumps over the lazy dog";
+let newString = sentence.substr1("Quick", "lazy")
+
+
+
+let sentence2 = "The quick brown fox"
+
+let newSentence2 = sentence2.split(sep = ",")
+console.log(newSentence2)
+
+
+
